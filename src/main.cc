b3b5d84352bac9acf17b495f4a9641461c93bc19
eigth-bit-guy <henriquedossantosq8d@gmail.com>
2024-08-08 18:11:32 -0300

    add sf::String and stringBuffer

--- src/main.cc
+++ src/main.cc
@@ -1,15 +1,18 @@
 #include <iostream>
+#include <vector>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window/Keyboard.hpp>
 
 int
 main(int argc, char **argv)
 {
+  sf::String stringBuffer;
   sf::Event event;
   sf::Text text;
   sf::Font font;
   sf::RenderWindow window;
   sf::Texture texture;
+  size_t interartorString = 0;
 
   const char *fontPath = "font/fantasquesansmono-regular.otf"; //add macro for this
   const char *texturePath = "font/bitmap_font/bitmap_font.png";
@@ -22,6 +25,10 @@ main(int argc, char **argv)
     std::cout << "INFO: error when load texture from file\n";
     return -1;
   }
+
+  text.setFont(font);
+  text.setCharacterSize(24);
+  text.setFillColor(sf::Color::Black);
   
   window.create(sf::VideoMode(800, 600), "TodoApp");
   sf::Vector2u vec = window.getSize();
@@ -40,13 +47,33 @@ main(int argc, char **argv)
 	std::cout << "INFO: close button pressed\n";
         window.close();
 	break;
+
+      case sf::Event::TextEntered:
+	std::cout << "INFO: entrada de texto: " << event.text.unicode << "\n";
+	if(event.text.unicode == 8){ //backspace
+	  stringBuffer.erase(interartorString - 1, 1);
+	  if(interartorString != 0){
+	    interartorString = interartorString - 1;
+	  }else
+	    break;
+	}else if(event.text.unicode == 13){
+	  stringBuffer.insert(interartorString, '\n');
+	  interartorString += 1;
+	}else{
+	  stringBuffer.insert(interartorString, (char)event.text.unicode);
+	  interartorString += 1;
+	}
+	text.setString(stringBuffer);
+	// std::cout << "interator size: " << interartorString << "\n";
+	// std::cout << "string buffer value: " << (std::string)stringBuffer << "\n";
+	break;
       }
     }
 
-    window.clear(sf::Color::Red);
+    window.clear(sf::Color::White);
+    window.draw(text);
     window.display();
   }
 
   return 0;
-
 } 
