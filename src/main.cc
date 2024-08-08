f92134d679637d4b7075d73193b4a34f6a4ef0b9
Samuel Henrique <henriquedossantosq8d@gmail.com>
2024-08-08 01:55:29 -0300

    add texture

--- src/main.cc
+++ src/main.cc
@@ -5,50 +5,46 @@
 int
 main(int argc, char **argv)
 {
-  // TextFont tf;
-  // Editor e;
-  // e.sb->count = 0;
-  // int err;
   sf::Event event;
   sf::Text text;
   sf::Font font;
-  sf::RenderWindow wd;
   sf::RenderWindow window;
+  sf::Texture texture;
 
   const char *fontPath = "font/fantasquesansmono-regular.otf"; //add macro for this
+  const char *texturePath = "font/bitmap_font/bitmap_font.png";
+
   if (!font.loadFromFile(fontPath)){
     std::cout << "Error: When load font from file\n";
     return -1;
   }
+  if(!texture.loadFromFile(texturePath)){
+    std::cout << "INFO: error when load texture from file\n";
+    return -1;
+  }
   
-  // err = tf.set_font(&text, &font);
-  // if(err == -1){
-  //   std::cout << "Error: set_text_and_font\n";
-  //   return -1;
-  // }
-  
-  wd.create(sf::VideoMode(800, 600), "TodoApp");
-  sf::Vector2u vec = wd.getSize();
+  window.create(sf::VideoMode(800, 600), "TodoApp");
+  sf::Vector2u vec = window.getSize();
   unsigned int wh = vec.x / 2;
   unsigned int hg = vec.y / 2;
-  wd.setPosition(sf::Vector2i(wh, hg));
-  wd.setVerticalSyncEnabled(true); // frame
+  window.setPosition(sf::Vector2i(wh, hg));
+  window.setVerticalSyncEnabled(true); // frame
 
-  while (wd.isOpen()) {
+  while (window.isOpen()) {
     
-    while (wd.pollEvent(event)) {
+    while (window.pollEvent(event)) {
       
       switch(event.type) {
 	    
       case sf::Event::Closed:
 	std::cout << "INFO: close button pressed\n";
-        wd.close();
+        window.close();
 	break;
       }
     }
 
-    wd.clear(sf::Color::Red);
-    wd.display();
+    window.clear(sf::Color::Red);
+    window.display();
   }
 
   return 0;
