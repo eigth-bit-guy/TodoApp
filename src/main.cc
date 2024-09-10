e4946f999b127485cfd62b4930010a4139a900ef
eigth-bit-guy <henriquedossantosq8d@gmail.com>
2024-09-09 23:19:53 -0300

    feat: themes folder for TGUI and main.cc modifications

--- src/main.cc
+++ src/main.cc
@@ -1,30 +1,51 @@
 #include <iostream>
 #include <TGUI/TGUI.hpp>
 #include <TGUI/Backend/SFML-Graphics.hpp>
+#define WIDTH 500
+#define HEIGHT 700
+#define WIDTH_BUTTON (WIDTH - 10)
+#define HEIGHT_BUTTON (HEIGHT - 10)
+
+void add_editbox(tgui::BackendGui& gui, tgui::Theme theme)
+{
+  auto editBox = tgui::EditBox::create();
+  editBox->setRenderer(theme.getRenderer("EditBox"));
+  editBox->setSize(200, 25);
+  editBox->setTextSize(18);
+  editBox->setPosition(10, 80);
+  editBox->setDefaultText("Title");
+  gui.add(editBox);
+}
 
 int main(int argc, char **argv)
 {
-  sf::RenderWindow window{{300, 600}, "TGUI example - SFML_GRAPHICS backend"};
+  sf::Mouse mouse;
+  sf::RenderWindow window{{WIDTH, HEIGHT}, "TodoApp"};
   tgui::Gui gui{window};
+  tgui::Theme theme{"themes/Black.txt"};
 
-  tgui::Button::Ptr button = tgui::Button::create();
-  auto editBox = tgui::EditBox::create();
+  auto button = tgui::Button::create();
+  button->setRenderer(theme.getRenderer("Button"));
+  button->setPosition(gui.getView().getSize().x - WIDTH_BUTTON,
+		      gui.getView().getSize().y - HEIGHT_BUTTON);
+  button->setText("Add new task");
+  button->setSize(100, 40);
   gui.add(button);
-  gui.add(editBox, "MyWidgetName");
 
-  while (window.isOpen())
-    {
-      sf::Event event;
-      while (window.pollEvent(event))
-	{
-	  gui.handleEvent(event);
-
-	  if (event.type == sf::Event::Closed)
-            window.close();
-	}
-
-      window.clear();
-      gui.draw();
-      window.display();
-    }  
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      gui.handleEvent(event);
+	  
+      if (event.type == sf::Event::Closed)
+	window.close();
+	  
+      if(event.type == mouse.isButtonPressed(sf::Mouse::Button::Left)){
+	add_editbox(gui, theme);
+      }
+    }
+    window.clear(sf::Color::Red);
+    gui.draw();
+    window.display();
+  }
 }
